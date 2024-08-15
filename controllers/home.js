451c744f2915d5c6f59317d3612ae9fe6042d83e
SDKwapis <165666421+SDKwapis@users.noreply.github.com>
2024-08-15 10:39:14 -0400

    added css to start building out front end, make it look pretty later

--- controllers/home.js
+++ controllers/home.js
@@ -1,7 +1,30 @@
 const router = require("express").Router();
 // this is just here as a test
 router.get("/", async (req, res) => {
-    res.render('home');
+  res.render('home', {
+    title: 'Home',
+    blogTitle: 'My Modern Blog',
+    aboutText: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus imperdiet, nulla et dictum interdum.',
+    posts: [
+        {
+            id: 1,
+            title: 'My First Blog Post',
+            date: 'August 15, 2024',
+            author: 'Author Name',
+            excerpt: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus imperdiet...',
+            image: 'https://via.placeholder.com/800x400'
+        },
+        {
+            id: 2,
+            title: 'Another Blog Post',
+            date: 'August 14, 2024',
+            author: 'Author Name',
+            excerpt: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus imperdiet...',
+            image: 'https://via.placeholder.com/800x400'
+        }
+    ],
+    categories: ['Category 1', 'Category 2', 'Category 3', 'Category 4']
+});
   });
   
   module.exports = router;
