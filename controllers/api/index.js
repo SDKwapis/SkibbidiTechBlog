b16e97ace091e1345c115a27630bb25efe0a86da
SDKwapis <165666421+SDKwapis@users.noreply.github.com>
2024-08-28 11:42:46 -0400

    added routes and model for users

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,4 +1,8 @@
 const router = require("express").Router();
+const userRoutes = require("./userRoutes");
+
+router.use("/users", userRoutes);
+
 // this is just here as a test
 router.get("/", async (req, res) => {
     res.send(`You're on api/index file`);
---
+++ models/User.js
@@ -0,0 +1,57 @@
+const {Model, DataTypes} = require('sequelize')
+const sequelize = require('../config/connection')
+const bcrypt = require('bcrypt');
+
+
+class User extends Model {
+    // Validates the password
+    checkPassword(loginPw) {
+      return bcrypt.compareSync(loginPw, this.password);
+    }
+  };
+// Definiton for User model 
+User.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+          },
+        name:{ 
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        email:{
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+              isEmail: true,
+            },
+        },
+        password:{
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [6],
+            },
+        },
+    },
+    {
+        hooks: {
+            // Hashes the users password before saving it to the DB
+          async beforeCreate(newUserData) {
+            newUserData.password = await bcrypt.hash(newUserData.password, 10);
+            return newUserData;
+          },
+        },
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'User',
+      }
+    );
+    
+    module.exports = User;
